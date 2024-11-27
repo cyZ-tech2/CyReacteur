26d78d1e4c1bd744a38d7c99e452a42b5bbca497
florian bruyant <e-fbruyant@prc-cy301-030.pedagogie.cyu.fr>
2024-11-27 14:27:49 +0100

    Creations structure et fonctions de base avl (WIP)

--- codeC/CyReacteur.h
+++ codeC/CyReacteur.h
@@ -1,4 +1,28 @@
+//bibliothèques
 #include <stdio.h>
 #include <stdlib.h>
 
+//Structures
 
+typedef struct arbre{
+	int el;
+	struct arbre* fg;
+	struct arbre* fd;
+	int eq;
+}Arbre;
+
+//Fonctions
+
+//AVL
+//Creation
+Arbre* creerArbre(int e);
+//Rotations simples
+Arbre* rotationGauche(Arbre* a);
+Arbre* rotationDroite(Arbre* a);
+//Rotations doubles
+Arbre* doubleRotationGauche(Arbre* a);
+Arbre* doubleRotationDroite(Arbre* a);
+//equilibrage
+Arbre* equilibrerAVL(Arbre* a);
+//insertion
+Arbre* insertionAVL(Arbre* a, int e, int* h)
---
+++ codeC/avl.c
@@ -0,0 +1,97 @@
+#include <CyReacteur.h>
+
+Arbre* creerArbre(int e){
+	Arbre* nouv = malloc(sizeof(Arbre));
+	nouv->el = e;
+	nouv->fg = NULL;
+	nouv->fd = NULL;
+	nouv->eq = 0;
+	return nouv;	
+}
+
+Arbre* rotationGauche(Arbre* a){
+	Arbre* pivot = a->fd;
+	int eq_a, eq_p;
+	a->fd = pivot->fg;
+	pivot->fg = a;
+	eq_a = a->eq;
+	eq_p = pivot->eq;
+	a->eq = eq_a - max(eq_p,0) - 1;
+	pivot->eq = min(eq_a-2, eq_a + eq_p-2, eq_p-1);
+	a = pivot;
+	return a;
+}
+
+Arbre* rotationDroite(Arbre* a){
+	Arbre* pivot = a->fg;
+	int eq_a, eq_p;
+	a->fg = pivot->fd;
+	pivot->fd = a;
+	eq_a = a->eq;
+	eq_p = pivot->eq;
+	a->eq = eq_a - min(eq_p,0) + 1;
+	pivot->eq = max(eq_a+2, eq_a + eq_p+2, eq_p+1);
+	a = pivot;
+	return a;
+}
+
+Arbre* doubleRotationGauche(Arbre* a){
+	a->fd = rotationDroite(a->fd);
+	return rotationGauche(a);
+}
+
+Arbre* doubleRotationDroite(Arbre* a){
+	a->fg = rotationDroite(a->fg);
+	return rotationDroite(a);
+}
+
+Arbre* equilibrerAVL(Arbre* a){
+	if(a->eq >= 2){
+		if(a->fd->eq >= 0){
+			return rotationGauche(a);
+		}
+		else{
+			return doubleRotationGauche(a);
+		}
+	}
+	else if(a->eq <= -2){
+		if(a->fg->eq <= 0){
+			return rotationDroite(a);
+		}
+		else{
+			return doubleRotationDroite(a);	
+		} 
+	}
+	return a;
+}
+
+Arbre* insertionAVL(Arbre* a, int e, int* h){
+	if(a==NULL){
+		*h=1;
+		return creerArbre(e);
+	}
+	else if(e < a->el){
+		a->fg = insertionAVL(a->fg, e,h);
+		*h=-*h;
+	}
+	else if(e > a->el){
+		a->fd = insertionAVL(a->fd, e,h);
+	}
+	else{
+		*h=0;
+		return a;
+	}
+	if(*h != 0){
+		a->eq = a->eq + *h;
+		a = equilibrerAVL(a);
+		if(a->eq == 0){
+			*h=0
+		}
+		else{
+			*h=1;
+		}
+	}
+	return a;
+}
+
+
