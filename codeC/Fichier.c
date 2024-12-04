b756e23256530ab7143005aa03d137a066b9ff82
z <98968648+cyZ-tech2@users.noreply.github.com>
2024-12-04 14:00:13 +0100

    Update Fichier.c

--- codeC/Fichier.c
+++ codeC/Fichier.c
@@ -1,1 +1,13 @@
-File*
+FILE* fichier = NULL;
+int caractereActuel = EOF+1;
+fichier = fopen("test.txt", "r");
+if (fichier != NULL)
+{
+while (caractereActuel != EOF)
+{
+caractereActuel = fgetc(fichier); // On lit le caractère
+printf("%c", caractereActuel); // On l'affiche
+} ;
+
+fclose(fichier);
+}
