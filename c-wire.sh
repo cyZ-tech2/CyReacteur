c293ec6a9f5f98aea167881b547cc6cfb4e3bca7
Florian <73788253+LeTaitre@users.noreply.github.com>
2024-12-12 21:42:22 +0100

    ajouts verifs args

--- c-wire.sh
+++ c-wire.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-listeArgs=&@
-
-if [ "$listeArgs" = '*-h*' ] ; then
+aide(){
 	#message d'aide
-fi
+	return 0
+}
 
-entree=&1
-station=&2
-conso=&3
+if [[ "$*" = *-h* ]] ; then # aide
+	aide
+	exit 0
+
+elif [[ "$1" != *.csv ]] ; then # verif fichier de donnees
+	echo "fichier d'entrée invalide"
+	aide
+	exit 1
+
+elif [ "$2" != "hvb" ] && [ "$2" != "hva" ] && [ "$2" != "lv" ] ; then   # verif type de station
+	echo "type de station invalide"
+	aide
+	exit 1
+
+elif [ "$3" != "comp" ] && [ "$3" != "indiv" ] && [ "$3" != "all" ] ; then # verif type de consommateur
+	echo "type de consommateur invalide"
+	aide
+	exit 1
+
+elif [[ "$*" = *hv?' 'all* ]] || [[ "$*" = *hv?' 'indiv* ]] ; then # verif option authorisée
+	echo "option non autorisée"
+	aide
+	exit 1
+fi
