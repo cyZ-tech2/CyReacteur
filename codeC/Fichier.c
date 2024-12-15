6c1629792d1a63962c48c2dab9e94077cab627f8
z <98968648+cyZ-tech2@users.noreply.github.com>
2024-12-15 13:58:23 +0100

    Update Fichier.c

--- codeC/Fichier.c
+++ codeC/Fichier.c
@@ -2,53 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_TAMPON 126 // Taille à voir 
+FILE* modifFichier(FILE* fichier){
+    if(fichier == NULL){
+        exit(1);
+    }
+    FILE* fichier_v2 = fopen("c-wire_v00.dat", "w");
+    if(fichier_v2 == NULL){
+        fclose(fichier);
+        exit(2);
+    }
+    char longueur[100];
+
+    while(fgets(longueur, sizeof(longueur), fichier) != NULL){
+    for (int i = 0; longueur[i] != '\0'; i++) {
+            if (longueur[i] == '-' && strchr(";\n\0", longueur[i + 1]) && (i == 0 || longueur[i - 1] == ';')) {
+                // Si '-' est isolé, remplacer par "-1"
+                fputc('-', fichier_v2);
+                fputc('1', fichier_v2);
+            }else {
+                fputc(longueur[i], fichier_v2);
+            }
+        }   
+    }
+    fclose(fichier);
+    fclose(fichier_v2);
+
+    
+    fichier_v2 = fopen("c-wire_v00.dat", "r");
+    if (fichier_v2 == NULL) {
+        exit(3);
+    }
+
+    return fichier_v2;
+}
 
 
 int main(){
 FILE* fichier = NULL;
-char longueur[MAX_TAMPON];
-
-fichier = fopen("c-wire_v00.dat", "r");
-if (fichier == NULL){
-    printf("Erreur fopen");
-    exit(1);
+FILE* fichier_v2 = NULL;
 }
 
-
-if(fgets(longueur, sizeof(longueur), fichier) == NULL){
-    printf("Erreur fgets");
-    fclose(fichier);
-    exit(2);
-}
-
-while(fgets(longueur, sizeof(longueur), fichier) != NULL){
-    unsigned long powerPlant, hvB, hvA, lv, capacity, load, individual, company;
-
-
-  int result = sscanf(longueur, "%lu;%lu;%lu;%lu;%lu;%lu;%lu;%lu", &powerPlant, &hvB, &hvA, &lv, company, individual, &capacity, &load);
-
-        if (result >= 6) {
-            
-            company[49] = '\0';
-            individual[49] = '\0';
-
-         
-            traiterDonnees(powerPlant, hvB, hvA, lv, capacity, load);
-        } else {
-            printf("Erreur traitement de données");
-        }
-    }
-    fclose(fichier);
-    exit(3);
-}
-
-void afficheDonnees(int powerPlant, int hvB, int hvA, int lv, int capacity, int load){
-    printf("")
-}
-
-void traiterDonnees(int powerPlant, int hvB, int hvA, int lv, int capacity, int load) {
-    if (capacity > 1000000000) {
-        printf("PowerPlant: %d, Capacity: %d, Load: %d\n", powerPlant, capacity, load);
-    }
-}
