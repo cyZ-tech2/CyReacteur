ec4b3908f0934dddf872097e36028fbd5ec73e16
z <98968648+cyZ-tech2@users.noreply.github.com>
2024-12-15 23:38:42 +0100

    Update and rename t to avl.h

---
+++ codeC/include/avl.h
@@ -0,0 +1,40 @@
+//bibliothèques
+#include <stdio.h>
+#include <stdlib.h>
+
+//Structures
+
+typedef struct donnees{
+	int id;
+	long unsigned centrale;
+    	long unsigned hvb;
+    	long unsigned hvb;
+    	long unsigned lv;
+    	long unsigned entrp;
+    	long unsigned partc;
+    	long unsigned conso;
+    	long unsigned produc;
+}Donnees;
+
+typedef struct arbre{
+	Donnees d;
+	struct arbre* fg;
+	struct arbre* fd;
+	int eq;	
+}Arbre;
+
+//Fonctions
+
+//AVL
+//Creation
+Arbre* creerArbre(int e, int a, int b);
+//Rotations simples
+Arbre* rotationGauche(Arbre* a);
+Arbre* rotationDroite(Arbre* a);
+//Rotations doubles
+Arbre* doubleRotationGauche(Arbre* a);
+Arbre* doubleRotationDroite(Arbre* a);
+//equilibrage
+Arbre* equilibrerAVL(Arbre* a);
+//insertion
+Arbre* insertionAVL(Arbre* a, int e, int* h);
