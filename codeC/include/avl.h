10806bf03717f05435abcf76fd944e6f333b541a
Florian <73788253+LeTaitre@users.noreply.github.com>
2024-12-22 23:39:15 +0100

    a

---
+++ codeC/include/avl.h
@@ -0,0 +1,38 @@
+//bibliothèques
+#include <stdio.h>
+#include <stdlib.h>
+
+//Structures
+typedef struct donnees {
+    unsigned long id;
+    unsigned long conso;
+    unsigned long produc;
+} Donnees;
+
+// Structure pour l'arbre AVL
+typedef struct arbre {
+    Donnees donnees;
+    struct arbre* fg;
+    struct arbre* fd;
+    int eq; // équilibre de l'arbre
+} Arbre;
+
+//AVL
+//Creation
+Arbre* creerArbre(Donnees d);
+//Rotations simples
+Arbre* rotationGauche(Arbre* a);
+Arbre* rotationDroite(Arbre* a);
+//Rotations doubles
+Arbre* doubleRotationGauche(Arbre* a);
+Arbre* doubleRotationDroite(Arbre* a);
+//equilibrage
+Arbre* equilibrerAVL(Arbre* a);
+//insertion
+Arbre* insertionAVL(Arbre* a, Donnees d, int* h);
+//supprime l'arbre
+void freeAVL(Arbre* a);
+//affiche l'arbre
+void afficherAVL(Arbre* a, FILE* fichier);
+// Fonction qui parcourt le fichier filtreConso et fait la somme des consommateurs pour chaque station
+void sommeConso(Arbre* AVLstation, Arbre* AVLconso);
