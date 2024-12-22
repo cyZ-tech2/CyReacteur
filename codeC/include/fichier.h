7c6cb1d885aeff7402a0811736044e244b3f6c64
z <98968648+cyZ-tech2@users.noreply.github.com>
2024-12-22 10:10:27 +0100

    comments

--- codeC/include/fichier.h
+++ codeC/include/fichier.h
@@ -10,5 +10,5 @@ Arbre* construireAVLStation(const char* cheminFichier);
 Arbre* construireAVLConso(const char* cheminFichier);
 // Fonction pour afficher l'AVL dans un fichier
 void AVLDansFichier(Arbre* a, const char* cheminFichier, const char* typeStation, const char* typeConso);
-
+// Fonction qui calcule la différence (production/conso)
 void diffCapConso(const char* cheminFichier);
